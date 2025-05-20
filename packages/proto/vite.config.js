5bcfa65c17a6c28a9dfbfec425248a14669c731d
Anthony <anthonymendoza321123@gmail.com>
2025-05-20 12:26:20 -0700

    added some bug fixed on deployed site

--- packages/proto/vite.config.js
+++ packages/proto/vite.config.js
@@ -8,13 +8,17 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        allTutorials: resolve(__dirname, 'all-tutorials.html'),
-        burpees: resolve(__dirname, 'tutorial-burpees.html'),
-        highKnees: resolve(__dirname, 'tutorial-high-knees.html'),
-        jumpingJacks: resolve(__dirname, 'tutorial-jumping-jacks.html'),
-        pushup: resolve(__dirname, 'tutorial-pushup.html'),
-        squat: resolve(__dirname, 'tutorial-squat.html')
+        index: resolve(__dirname, "index.html"),
+        allTutorials: resolve(__dirname, "all-tutorials.html"),
+        strength: resolve(__dirname, "workout-plan-strength.html"),
+        cardio: resolve(__dirname, "workout-plan-cardio.html"),
+        custom: resolve(__dirname, "workout-plan-custom.html"),
+        pushup: resolve(__dirname, "tutorial-pushup.html"),
+        squat: resolve(__dirname, "tutorial-squat.html"),
+        burpees: resolve(__dirname, "tutorial-burpees.html"),
+        jumpingJacks: resolve(__dirname, "tutorial-jumping-jacks.html"),
+        highKnees: resolve(__dirname, "tutorial-high-knees.html"),
+        achievement: resolve(__dirname, "achievement-first-workout.html")
       }
     }
   }
